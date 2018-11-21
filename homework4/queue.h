260ada5b7e89369724dce5f43fc54cb5d2037eae
Brett Derham <brettderham@Bretts-MacBook-Pro.local>
2018-11-20 21:57:51 -0800

    Finish Homework

--- homework4/queue.h
+++ homework4/queue.h
@@ -1,71 +1,69 @@
 #include<iostream>
 #include<ostream>
 using namespace std;
-// Creating template Node class
+
 template<class T>
 class Node{
-public:
-T data;
-Node<T> *next;
-Node<T>(T data){
-this->data = data;
-next = NULL;
-}
-};
-//Declaration of Queue class
+  public:
+    T data;
+    Node<T> *next;
+    Node<T>(T data){
+      this->data = data;
+      next = NULL;
+    }
+  };
+
 template <typename> class Queue;
-// Declaration of << overloading function
+
 template <typename T> std::ostream& operator<<(std::ostream&,Queue<T>);
-// Implementation of Queue class
+
 template <typename T>
-class Queue
-{
-int size;
-Node<T> *head, *tail;
+class Queue {
+  int size;
+  Node<T> *head, *tail;
 public:
-Queue(){
-size=0;
-head=NULL;
-tail=NULL;
+  Queue(){
+    size=0;
+    head=NULL;
+    tail=NULL;
 }
 int get_size(){
-return size;
+  return size;
 }
+
 void enqueue(T data){
-Node<T> *node = new Node<T>(data);
-if(size == 0){
-head=node;
-tail=node;
-} else{
-tail->next = node;
-tail = tail->next;
-}
-size++;
+  Node<T> *node = new Node<T>(data);
+  if(size == 0){
+    head=node;
+    tail=node;
+  } else {
+    tail->next = node;
+    tail = tail->next;
+  }
+  size++;
 }
 T dequeue(){
-if(size==0){
-throw std::underflow_error("underflow_error");
+  if(size==0){
+    throw std::underflow_error("underflow_error");
+  } else {
+    T data = head->data;
+    if(head == tail){
+      head = tail = NULL;
+    } else {
+      head = head->next;
+    }
+    size--;
+    return data;
+  }
 }
-else{
-T data = head->data;
-if(head == tail){
-head = tail = NULL;
-} else{
-head=head->next;
-}
-size--;
-return data;
-}
-}
-// making operator overloading function as friend of Queue class
 friend std::ostream& operator<< <>(std::ostream& out,Queue<T> obj);
 };
-// Implementation of << overloading function
+
 template <typename T> std::ostream& operator<<(std::ostream& out,Queue<T> obj){
-Node<T> *tmp = obj.head;
-while(tmp != NULL){
-out<<tmp->data<<" ";
-tmp = tmp->next;
-}
-return out;
+  Node<T> *tmp = obj.head;
+  while(tmp != NULL){
+    out<<tmp->data<<" ";
+    tmp = tmp->next;
+  }
+  return out;
 };
--- homework4/say.cpp
+++ homework4/say.cpp
@@ -11,29 +11,18 @@ public:
 };
 Test Test::say(string word)
 {
-//If word is emplty print the whole array
   if(word.empty()){
-
       for(int i =0 ;i<total_words;i++){
           cout<<array[i]<<" ";
-
       }
-
+  } else {
+    array[total_words] = word;
+    ++total_words;
   }
-//otherwise store the passed word in array
-  else{
-      array[total_words] = word;
-      ++total_words;
-
-  }
-
-  return *this; // Return this pointer
+  return *this;
 }
 
-int main()
-{
+int main() {
   Test Obj;
-
   Obj.say("Hello").say("my").say("name").say("is").say("Colette").say();
-
 }
